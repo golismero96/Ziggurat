8fb1b5caa97980769907da9b8bd275f208880c8f
golismero <golismero96@gmail.com>
2023-08-12 23:32:18 +0330

    fix dark mode

--- src/theme/ThemeProvider.tsx
+++ src/theme/ThemeProvider.tsx
@@ -1,23 +1,51 @@
 import { ThemeProvider } from '@mui/system';
 import { themeCreator } from './base';
-import { ThemeUIProvider as ThemeUIContextProvider } from './ThemeContext';
-import React, { FC, useState } from 'react';
+import {
+  ThemeContextInterface,
+  ThemeUIProvider as ThemeUIContextProvider
+} from './ThemeContext';
+import React, { FC, useMemo, useState } from 'react';
+import { PaletteMode } from '@mui/material';
 
 const ThemeProviderWrapper: FC<any> = (props) => {
-  const MainTheme = themeCreator('PureLightTheme');
+  // const MainTheme = themeCreator('PureLightTheme');
 
-  // const themeUIEvents: ThemeContextInterface = {
-  //   toggleMode: (mode: PaletteMode) => {
-  //     console.log('22222222');
-  //     theme.palette.mode = mode;
-  //   }
-  // };
+  const [themeName, _setThemeName] = useState(themeCreator('PureLightTheme'));
+
+  const themeUIEvents: ThemeContextInterface = {
+    toggleMode: (mode: PaletteMode) => {
+      _setThemeName({
+        ...themeName,
+        palette: {
+          ...themeName.palette,
+          mode: mode
+        }
+      });
+    }
+  };
+
+  // const { themeMode, language } = useThemeMode();
+
+  // useEffect(() => {
+  //   console.log('themeMode1', theme.palette.mode);
+  //   theme.palette.mode = themeMode;
+  //   console.log('themeMode2', theme.palette.mode);
+
+  // const theme = useMemo(
+  //   () =>
+  //     createTheme({
+  //       palette: {
+  //         mode,
+  //       },
+  //     }),
+  //   [mode]
+  // );
+  //
+  // }, [themeMode]);
 
   return (
-    <ThemeUIContextProvider>
-      {' '}
-      {/* ThemeUIEvents={themeUIEvents} */}
-      <ThemeProvider theme={MainTheme}>{props.children}</ThemeProvider>
+    <ThemeUIContextProvider ThemeUIEvents={themeUIEvents}>
+      <ThemeProvider theme={themeName}>{props.children}</ThemeProvider>
     </ThemeUIContextProvider>
   );
 };
